42522bd1e5643b470e269dee0fbb87b52c1be0eb
DALM1 <92697916+DALM1@users.noreply.github.com>
2023-02-11 22:57:33 +0100

    Updates

--- scraping5.py
+++ scraping5.py
@@ -3,20 +3,20 @@ from bs4 import BeautifulSoup
 
 # Informations de login
 payload = {
-    "username": "",
-    "password": ""
+    "username": "myusername",
+    "password": "mypassword"
 }
 
 # Envoi de la requête POST pour effectuer le login
 with requests.Session() as session:
-    session.post("...", data=payload)
+    session.post("https://www.example.com/login", data=payload)
     
     # Accès à la page web protégée par login
-    response = session.get("...")
+    response = session.get("https://www.example.com/protected_page")
     
     # Traitement du contenu HTML pour en extraire les informations souhaitées
     soup = BeautifulSoup(response.content, "html.parser")
-    results = soup.find_all("src", class_="userChat")
+    results = soup.find_all("div", class_="result")
     
     # Afficher les résultats
     for result in results:
