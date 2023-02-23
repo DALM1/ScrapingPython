8a28f50130a63f048356583818790016a72294df
DALM1 <92697916+DALM1@users.noreply.github.com>
2023-02-23 01:24:04 +0100

    Updates steg001

--- steg001.py
+++ steg001.py
@@ -17,7 +17,7 @@ def embed_text_in_image():
 
     text_file_name = f"{os.path.splitext(last_image)[0]}.txt"
     with open(text_file_name, "w") as text_file:
-        print("Enter the message to hide:")
+        print("Entrez le message à cacher :")
         message = input().strip()
         text_file.write(message)
 
@@ -25,5 +25,30 @@ def embed_text_in_image():
     os.remove(text_file_name)
     print(f"Message caché dans {last_image} et {text_file_name} supprimé.")
 
+def extract_text_from_image():
+    all_files = []
+    for dirpath, dirnames, filenames in os.walk("."):
+        for filename in filenames:
+            if filename.endswith(".jpg"):
+                all_files.append(os.path.join(dirpath, filename))
+
+    if not all_files:
+        print("Aucune image trouvée")
+        return
+
+    last_image = max(all_files, key=os.path.getmtime)
+    print(f"Dernière image trouvée : {last_image}")
+
+    subprocess.run(["steghide", "extract", "-sf", last_image])
+    text_file_name = f"{os.path.splitext(last_image)[0]}.txt"
+
+    with open(text_file_name, "r") as text_file:
+        message = text_file.read()
+        print(f"Message caché dans {last_image} : {message}")
+
+    os.remove(text_file_name)
+    print(f"Fichier {text_file_name} supprimé.")
+
 if __name__ == "__main__":
     embed_text_in_image()
+    extract_text_from_image()
